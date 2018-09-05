825c4aed1ed18bda576babfc718bc734a27bb12e
insanemax <tm_4@mail.ru>
2018-09-05 17:48:19 +0300

    beatify code, write some functions

--- js/stat.js
+++ js/stat.js
@@ -5,6 +5,13 @@ var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var GAP = 10;
+var FONT_GAP = 15;
+var BAR_GAP = 50;
+var BAR_WIDTH = 40;
+var BAR_HEIGHT = 150;
+var TEXT_WIDTH = 80;
+var barY = CLOUD_HEIGHT - GAP * 4 - FONT_GAP * 3;
+
 
 function renderCloud(ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -13,32 +20,39 @@ function renderCloud(ctx, x, y, color) {
   ctx.fillRect(x - GAP, y - GAP, CLOUD_WIDTH, CLOUD_HEIGHT);
 }
 
+function renderText(ctx, font, color, text1, text2) {
+  ctx.font = font;
+  ctx.fillStyle = color;
+  ctx.fillText(text1, CLOUD_X + GAP * 4, CLOUD_Y + GAP * 2);
+  ctx.fillText(text2, CLOUD_X + GAP * 4, CLOUD_Y + FONT_GAP + GAP * 3);
+}
+
+//начало функции, которую я не понял)
+
+function renderColumn(name, time, height) {
+
+}
+
 function getMaxOfArray(nameOfArray) {
   return Math.max.apply(null, nameOfArray);
 }
 
+function randomNumber() {
+  return Math.floor(Math.random() * 255);
+}
+
+function getRandomBlue() {
+  return 'rgb(' + '0, 0, ' + randomNumber() + ')';
+}
+
 window.renderStatistics = function (ctx, names, times) {
-  var FONT_GAP = 15;
-  var BAR_GAP = 50;
-  var BAR_WIDTH = 40;
-  var BAR_HEIGHT = 150;
-  var TEXT_WIDTH = 80;
-  var barY = CLOUD_HEIGHT - GAP * 4 - FONT_GAP * 3;
   var maxValue = getMaxOfArray(times);
 
-  function getRandomBlue() {
-    return 'rgb(' + '0, 0, ' + randomNumber + ')';
-  }
-
   renderCloud(ctx, CLOUD_X, CLOUD_Y, 'rgba(0, 0, 0, 0.7)');
-
-  ctx.font = '16px PT Mono';
-  ctx.fillStyle = '#000000';
-  ctx.fillText('Ура вы победили!', CLOUD_X + GAP * 4, CLOUD_Y + GAP * 2);
-  ctx.fillText('Список результатов:', CLOUD_X + GAP * 4, CLOUD_Y + FONT_GAP + GAP * 3);
+  renderText(ctx, '16px PT Mono', '#000000', 'Ура вы победили!', 'Список результатов:')
 
   for (var i = 0; i < names.length; i++) {
-    var randomNumber = Math.floor(Math.random() * 255);
+
     times[i] = Math.floor(times[i]);
     ctx.fillStyle = getRandomBlue();
     if (names[i] === 'Вы') {
