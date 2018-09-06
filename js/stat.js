5af4e19f20ee028f0aac35ab524f3319c16a3a64
insanemax <tm_4@mail.ru>
2018-09-06 12:09:12 +0300

    доработал функционал выводы статистики

--- js/stat.js
+++ js/stat.js
@@ -9,9 +9,20 @@ var FONT_GAP = 15;
 var BAR_GAP = 50;
 var BAR_WIDTH = 40;
 var BAR_HEIGHT = 150;
-var TEXT_WIDTH = 80;
-var barY = CLOUD_HEIGHT - GAP * 4 - FONT_GAP * 3;
+var OFFSET_TEXT_Y = 15;
+var OFFSET_Y = 90;
 
+function getMaxOfArray(nameOfArray) {
+  return Math.max.apply(null, nameOfArray);
+}
+
+function randomNumber() {
+  return Math.floor(Math.random() * 255);
+}
+
+function getRandomBlue() {
+  return 'rgb(' + '0, 0, ' + randomNumber() + ')';
+}
 
 function renderCloud(ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -27,39 +38,28 @@ function renderText(ctx, font, color, text1, text2) {
   ctx.fillText(text2, CLOUD_X + GAP * 4, CLOUD_Y + FONT_GAP + GAP * 3);
 }
 
-//начало функции, которую я не понял)
-
-function renderColumn(name, time, height) {
-
-}
-
-function getMaxOfArray(nameOfArray) {
-  return Math.max.apply(null, nameOfArray);
-}
-
-function randomNumber() {
-  return Math.floor(Math.random() * 255);
-}
-
-function getRandomBlue() {
-  return 'rgb(' + '0, 0, ' + randomNumber() + ')';
+function renderColumn(ctx, name, time, x, y, height) {
+  if (name === 'Вы') {
+    ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+  }
+  ctx.fillText(name, x, y - OFFSET_TEXT_Y);
+  ctx.fillRect(x, y, BAR_WIDTH, height);
+  ctx.fillText(time, x, y + height + OFFSET_TEXT_Y);
 }
 
 window.renderStatistics = function (ctx, names, times) {
   var maxValue = getMaxOfArray(times);
 
   renderCloud(ctx, CLOUD_X, CLOUD_Y, 'rgba(0, 0, 0, 0.7)');
-  renderText(ctx, '16px PT Mono', '#000000', 'Ура вы победили!', 'Список результатов:')
+  renderText(ctx, '16px PT Mono', '#000000', 'Ура вы победили!', 'Список результатов:');
 
   for (var i = 0; i < names.length; i++) {
-
-    times[i] = Math.floor(times[i]);
+    var x = CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i;
+    var height = times[i] / maxValue * BAR_HEIGHT;
+    var y = OFFSET_Y + BAR_HEIGHT - height;
+    var time = Math.floor(times[i]);
+    var name = names[i];
     ctx.fillStyle = getRandomBlue();
-    if (names[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    }
-    ctx.fillText(names[i], CLOUD_X + GAP * 3 + (TEXT_WIDTH + GAP) * i, CLOUD_HEIGHT - GAP);
-    ctx.fillRect(CLOUD_X + GAP * 3 + (BAR_WIDTH + BAR_GAP) * i, (GAP * 3) + FONT_GAP * 2 + barY, BAR_WIDTH, BAR_HEIGHT * times[i] * -1 / maxValue);
-    ctx.fillText(times[i], CLOUD_X + GAP * 3 + (TEXT_WIDTH + GAP) * i, (BAR_HEIGHT * times[i] * -1 / maxValue) + barY + FONT_GAP * 2 + GAP * 2);
+    renderColumn(ctx, name, time, x, y, height);
   }
 };
